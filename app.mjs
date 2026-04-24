405f45430c4ba7e5967acecaf0fd042b6a095899
MAFERCARRE <mafercarrenio@gmail.com>
2026-04-24 19:59:04 -0300

    cree archivo de rutas de frontend

--- app.mjs
+++ app.mjs
@@ -5,14 +5,20 @@ import superHeroRoutes from './routes/superHeroesRoutes.mjs';
 const app = express();
 const PORT = process.env.PORT||3000;
 
-//Middleware para parsear JSON
+//Middleware para parsear JSON (Mid para que las solicitudes se conviertan a JSON automáticamente)
 app.use (express.json());
+
+//Configurar ejs como motor de plantillas
+app.set("view engine","ejs");
 
 //Conexión a MongoDB
 connectDB();
 
-//Configuración de rutas
+//Configuración de rutas- Rutas backend para la api de superheroes
 app.use('/api', superHeroRoutes);
+
+//ruta frontend/plantillas pare el dashboard de heroes
+app.use("/dashboard", heroesroutefront);
 
 //Manejo de errores para rutas no encontradas
 app.use((req,res)=>{
---
+++ routes/superHeroFront.mjs
@@ -0,0 +1,24 @@
+import express from "express";
+import { getDashboardController} from "../controllers/superheroesControllers.mjs";
+
+const router= express.Router();
+//Ruta para mostrar el dashboard con la lista de heroes desde el controlador nuevo
+router.get("/", getDashboardController);
+
+//Ruta para mostrar el formulario de creación de un nuevo heroe
+router.get("/nuevo", (req, res)=> {
+    res.render("addSuperhero");
+});
+
+//Ruta para mostrar el formulario de edición de un heroe
+router.get("/modificar/:id", async(req, res)=>{
+    const respuesta=await fetch(
+        'http://localhost3000/api/heroes/id/${req.params.id}',
+    );
+    const heroe=respuesta.json();
+    if(!hero){
+        return res.status(404).send("Superheroe no encontrado");
+    }
+    res.render("editSuperhero",{heroe});
+});
+export default router;
