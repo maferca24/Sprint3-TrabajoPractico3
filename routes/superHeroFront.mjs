b77ee0f8633e5e099722c4e9ef763de5d8d8bb97
maferca24 <mafercarrenio@gmail.com>
2026-04-28 09:54:17 -0300

    depure archivo superHeroFront

--- routes/superHeroFront.mjs
+++ routes/superHeroFront.mjs
@@ -29,37 +29,6 @@ router.get("/modificar/:id", async (req, res) => {
         res.status(500).send("Error interno del servidor");
     }
 });
-// Ruta para mostrar el formulario de edición de un heroe
-// router.get("/modificar/:id", async (req, res) => {
-//     try {
-//         // 1. Usamos backticks `` para que ${req.params.id} funcione
-//         const respuesta = await fetch(`http://dashboard/${req.params.id}`); 
-        
-//         // 2. IMPORTANTE: Debes usar await aquí también
-//         const heroe = await respuesta.json(); 
-
-//         // 3. Verificamos si el héroe existe (usando la variable correcta 'heroe')
-//         if (!heroe) {
-//             return res.status(404).send("Superheroe no encontrado");
-//         }
-
-//         res.render("editSuperhero", { heroe });
-//     } catch (error) {
-//         console.error("Error al obtener el héroe:", error);
-//         res.status(500).send("Error interno del servidor");
-//     }
-// });
-
-//Ruta para mostrar el formulario de edición de un heroe
-// router.get("/modificar/:id", async (req, res) => {
-//     const respuesta = await fetch(
-//         'http://localhost:3000/dashboard/modificar/${req.params.id}');
-//     const heroe = respuesta.json();
-//     if (!hero) {
-//         return res.status(404).send("Superheroe no encontrado");
-//     }
-//     res.render("editSuperhero", { heroe });
-// });
 
 export default router;
 
